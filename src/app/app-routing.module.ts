da2c396b537c8464bee385e3082f4040cd844ac8
Fercho <efordonez7@utpl.edu.ec>
2024-11-20 15:51:16 -0500

    Migración del proyecto al repositorio Arte Latino

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { AboutComponent } from './pages/about/about.component';
 import { ProyectosComponent } from './pages/proyectos/proyectos.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { EquipoComponent } from './pages/equipo/equipo.component';
-import { NuevosProyectosComponent } from './pages/nuevos-proyectos/nuevos-proyectos.component';
 import { AcademyComponent } from './academy/academy.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { ProjectLayoutComponent } from './pages/project-layout/project-layout.component';
